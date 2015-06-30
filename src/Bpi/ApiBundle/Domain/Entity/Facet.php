07a597e42f11c1ce235029fa912bc5a98c1f1437
MM <mm@inlead.dk>
2015-06-30 14:49:46 +0300

    BPI-50 - Create facet entity and repository. (cherry picked from commit c965a2b)

---
+++ src/Bpi/ApiBundle/Domain/Entity/Facet.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Bpi\ApiBundle\Domain\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+
+
+/**
+ * Bpi\ApiBundle\Domain\Entity\Facet
+ */
+class Facet
+{
+    /**
+     * @var MongoId $id
+     */
+    protected $id;
+
+    /**
+     * @var string $nodeId
+     */
+    protected $nodeId;
+
+    /**
+     * @var collection $facetData
+     */
+    protected $facetData;
+
+
+    public function __construct()
+    {
+        $this->facetData = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nodeId
+     *
+     * @param string $nodeId
+     * @return self
+     */
+    public function setNodeId($nodeId)
+    {
+        $this->nodeId = $nodeId;
+        return $this;
+    }
+
+    /**
+     * Get nodeId
+     *
+     * @return string $nodeId
+     */
+    public function getNodeId()
+    {
+        return $this->nodeId;
+    }
+
+    /**
+     * Set facetData
+     *
+     * @param collection $facetData
+     * @return self
+     */
+    public function setFacetData($facetData)
+    {
+        $this->facetData = $facetData;
+        return $this;
+    }
+
+    /**
+     * Get facetData
+     *
+     * @return collection $facetData
+     */
+    public function getFacetData()
+    {
+        return $this->facetData;
+    }
+}
