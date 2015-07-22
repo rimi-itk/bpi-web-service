90dae1688afc3b89f4bc403864e07685278b2d27
MM <mm@inled.dk>
2015-07-22 12:26:42 +0300

    Add test coverage to test.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo y | php app/console doctrine:mongodb:fixtures:load
 export BPI_TEST_ENDPOINT_URI="http://bpi-ws.ci.inlead.dk/"
-./vendor/bin/phpunit -c app/phpunit.xml src/Bpi/ApiBundle/Tests/
+./vendor/bin/phpunit --coverage-html=./testcoverage -c app/phpunit.xml src/Bpi/ApiBundle/Tests/
