6c5c33817b9e04d88ea695843cae8c3584c87519
MM <mm@inled.dk>
2015-08-05 14:50:59 +0300

    BPI-90 - Make batch for facet creation.

--- src/Bpi/ApiBundle/Resources/config/Migrations/MongoDB/Version20150804143240.php
+++ src/Bpi/ApiBundle/Resources/config/Migrations/MongoDB/Version20150804143240.php
@@ -6,6 +6,7 @@ use AntiMattr\MongoDB\Migrations\AbstractMigration;
 use Doctrine\MongoDB\Database;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Bpi\ApiBundle\Domain\Entity\Facet;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -29,14 +30,78 @@ class Version20150804143240 extends AbstractMigration implements ContainerAwareI
 
     public function up(Database $db)
     {
+        $batchSize = 50;
+        $i = 0;
         $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
         $nodeRepository = $dm->getRepository('BpiApiBundle:Aggregate\Node');
 
         $nodes = $nodeRepository->findAll();
-        $facetRepository = $dm->getRepository('BpiApiBundle:Entity\Facet');
         foreach ($nodes as $node) {
-            $facetRepository->prepareFacet($node);
+            $facet = new Facet();
+
+            $agencyId = $node
+                ->getAuthor()
+                ->getAgencyId()
+            ;
+
+            $agency = $dm
+                ->getRepository('BpiApiBundle:Aggregate\Agency')
+                ->findOneBy(array('public_id' => $agencyId->id()))
+            ;
+
+            $categoryId = $node
+                ->getCategory()
+                ->getId()
+            ;
+            $category = $dm
+                ->getRepository('BpiApiBundle:Entity\Category')
+                ->findOneBy(array('_id' => $categoryId))
+                ->getCategory()
+            ;
+
+            $audienceId = $node
+                ->getAudience()
+                ->getId()
+            ;
+            $audience = $dm
+                ->getRepository('BpiApiBundle:Entity\Audience')
+                ->findOneBy(array('_id' => $audienceId))
+                ->getAudience()
+            ;
+
+            $tags = array();
+            $nodeTags = $node->getTags();
+            foreach ($nodeTags as $key => $tag) {
+                $tags[] = $tag->getTag();
+            }
+
+            $facets = array(
+                'agency_id' => array($agencyId->id()),
+                'agency_internal' => array($agency->getInternal()),
+                'category' => array($category),
+                'audience' => array($audience),
+                'tags' => array($tags),
+            );
+
+            $setFacets = new \stdClass();
+            array_walk($facets, function ($facet, $key) use (&$setFacets) {
+                if (!empty($facet)) {
+                    $setFacets->$key = $facet[0];
+                }
+            });
+
+            $facet->setNodeId($node->getId());
+            $facet->setFacetData($setFacets);
+
+            $dm->persist($facet);
+
+            if (($i % $batchSize) === 0) {
+                $dm->flush();
+                $dm->clear();
+            }
+            ++$i;
         }
+        $dm->flush();
     }
 
     public function down(Database $db)
