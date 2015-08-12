c7abbd45d7caa09bd180561b6a545d3582803ddd
AV <av@inlead.dk>
2015-08-12 12:33:03 +0300

    BPI-84 - Fix.

--- src/Bpi/ApiBundle/Controller/ChannelController.php
+++ src/Bpi/ApiBundle/Controller/ChannelController.php
@@ -317,8 +317,7 @@ class ChannelController extends BPIController
             }
 
             $nodes = $channel->getChannelNodes();
-            if ($nodes->contains($node))
-            {
+            if ($nodes->contains($node)) {
                 $skipped[] = $node->getId();
                 continue;
             }
