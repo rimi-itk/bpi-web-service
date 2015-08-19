d064016518b264978a423635861aed7e82b9d1c9
MM <mm@inlead.dk>
2015-08-19 10:45:15 +0300

    BPI-57 - Update modification time, bigger fields for teaser/body, create time disabled. (cherry picked from commit 4f0f286)

--- src/Bpi/AdminBundle/Controller/NodeController.php
+++ src/Bpi/AdminBundle/Controller/NodeController.php
@@ -76,9 +76,16 @@ class NodeController extends Controller
         $form = $this->createNodeForm($node);
         $request = $this->getRequest();
 
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
+        $uow = $dm->getUnitOfWork();
+        $uow->computeChangeSets();
+
         if ($request->isMethod('POST')) {
             $form->bind($request);
             if ($form->isValid()) {
+                $changeSet = $uow->getDocumentChangeSet($node);
+                $modifyTime = new \DateTime();
+                $node->setMtime($modifyTime);
                 $this->getRepository()->save($node);
                 return $this->redirect(
                     $this->generateUrl('bpi_admin_node')
@@ -147,7 +154,8 @@ class NodeController extends Controller
                 'text',
                 array(
                     'label' => 'Author agency id',
-                    'required' => true
+                    'required' => true,
+                    'disabled' => true
                 )
             )
             ->add(
@@ -168,7 +176,8 @@ class NodeController extends Controller
                     'label' => 'Modify time',
                     'required' => true,
                     'date_widget' => 'single_text',
-                    'time_widget' => 'single_text'
+                    'time_widget' => 'single_text',
+                    'disabled' => true
                 )
             )
             ->add('title', 'text')
