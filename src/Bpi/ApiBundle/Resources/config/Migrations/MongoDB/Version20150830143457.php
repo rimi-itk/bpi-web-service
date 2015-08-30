344b8dad3d5375a91d92a30b35f1a62e162aa08f
MM <mm@inled.dk>
2015-08-30 14:36:14 +0300

    BPI-95 - Create migrateion class.

---
+++ src/Bpi/ApiBundle/Resources/config/Migrations/MongoDB/Version20150830143457.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bpi\ApiBundle\Migrations\MongoDB;
+
+use AntiMattr\MongoDB\Migrations\AbstractMigration;
+use Doctrine\MongoDB\Database;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150830143457 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return "";
+    }
+
+    public function up(Database $db)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Database $db)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
